a5adc7f38a3fd5f06f06495646edc8c88dc6ef59
sushmajk <45619698+sushmajk@users.noreply.github.com>
2022-06-03 14:37:10 +0530

    Add files via upload

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,8 +1,13 @@
 function menuScroll() {
     var element = document.getElementById("page");
+    var element2 = document.getElementById("hide");
     element.classList.add("menuScroll");
+    element2.classList.add("hide");
   }
   function startParentScroll() {
     var element = document.getElementById("page");
+    var element2 = document.getElementById("hide");
     element.classList.remove("menuScroll");
+    element2.classList.remove("hide");
   }
+
